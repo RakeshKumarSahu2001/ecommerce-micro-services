a7173ef7ad2c7900f84004e17f138108cf123a87
RakeshKumarSahu2001 <rakeshkumarsahu187@gmail.com>
2025-07-07 21:13:57 +0530

    started docekrizing

--- products/src/middlewares/auth.middleware.js
+++ products/src/middlewares/auth.middleware.js
@@ -1,24 +1,57 @@
 import axios from "axios";
 import ApiError from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+
+// async function auth(req, res, next) {
+//   try {
+//     const token = req.cookies?.accessToken || req.header("Authorization").split(" ")[1];
+//     const response = await axios.get(
+//       `${process.env.AUTH_BASE_URL + "/customer/v1/user-info"}`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       }
+//     );
+    
+//     const user=response.data;
+//     if(!user){
+//         throw new ApiError(401,"Unautherized user...","Unautherized user...");
+//     }
+
+//     req.user=response.data.data;
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+
+// export default auth;
+
 
 async function auth(req, res, next) {
   try {
-    const token = req.cookies?.accessToken || req.header("Authorization").split(" ")[1];
-    const response = await axios.get(
-      `${process.env.AUTH_BASE_URL + "/customer/v1/user-info"}`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    
-    const user=response.data;
-    if(!user){
-        throw new ApiError(401,"Unautherized user...","Unautherized user...");
-    }
+    const token =
+      req.cookies?.accessToken || req.header("Authorization").split(" ")[1];
 
-    req.user=response.data.data;
+    if (!token || token === "" || token === null) {
+      throw new ApiError(
+        401,
+        "Unautherized request...",
+        "Unautherized request..."
+      );
+    }
+    const decodedData = await jwt.verify(
+      token,
+      process.env.ACCESSTOKEN_SECRETE
+    );
+
+    if (!decodedData) {
+      throw new ApiError(401, "Invalid User", "Invalid User");
+    }
+    req.user=decodedData;
+    console.log("decodedData",decodedData)
+
     next();
   } catch (error) {
     next(error);
--- shopping/src/middlewares/auth.middleware.js
+++ shopping/src/middlewares/auth.middleware.js
@@ -1,29 +1,63 @@
 import ApiError from "../utils/ApiError.js";
 import axios from "axios";
+import jwt from "jsonwebtoken";
+
+// async function auth(req, res, next) {
+//   try {
+//     const token =
+//       req.cookies?.accessToken || req.header("Authorization").split(" ")[1];
+
+//       if (!token) {
+//       throw new ApiError(401, "Unautherized User...", "Unautherized User...");
+//     }
+
+//     const response = await axios.get(
+//       `${process.env.AUTH_BASE_URL + "/customer/v1/user-info"}`,
+//       {
+//         headers: {
+//           Authorization: `${"Bearer " + token}`,
+//         },
+//       }
+//     );
+//     const user = response.data;
+//     if (!user) {
+//       throw new ApiError(401, "Unautherized user...", "Unautherized user...");
+//     }
+
+//     req.user = response.data.data;
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+
+// export default auth;
+
+
 
 async function auth(req, res, next) {
   try {
     const token =
       req.cookies?.accessToken || req.header("Authorization").split(" ")[1];
 
-      if (!token) {
-      throw new ApiError(401, "Unautherized User...", "Unautherized User...");
+    if (!token || token === "" || token === null) {
+      throw new ApiError(
+        401,
+        "Unautherized request...",
+        "Unautherized request..."
+      );
     }
-
-    const response = await axios.get(
-      `${process.env.AUTH_BASE_URL + "/customer/v1/user-info"}`,
-      {
-        headers: {
-          Authorization: `${"Bearer " + token}`,
-        },
-      }
+    const decodedData = await jwt.verify(
+      token,
+      process.env.ACCESSTOKEN_SECRETE
     );
-    const user = response.data;
-    if (!user) {
-      throw new ApiError(401, "Unautherized user...", "Unautherized user...");
-    }
 
-    req.user = response.data.data;
+    if (!decodedData) {
+      throw new ApiError(401, "Invalid User", "Invalid User");
+    }
+    req.user=decodedData;
+
+    console.log("decodedData",decodedData)
     next();
   } catch (error) {
     next(error);
